85cbf8f93f711ac65fd35e3750eaf0f9f98f986e
Meghanahl <53852111+Meghanahl@users.noreply.github.com>
2019-11-20 12:31:49 +0530

    Create lab10b.cpp

---
+++ lab10b.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+#define V 5
+  
+#define INF 9999
+void printSolution(int dist[][V]);  
+  
+void floydWarshall (int graph[][V])  
+{  
+   
+    int dist[V][V], i, j, k;  
+  
+    
+    for (i = 0; i < V; i++)  
+        for (j = 0; j < V; j++)  
+            dist[i][j] = graph[i][j];  
+  
+   
+    for (k = 0; k < V; k++)  
+    {  
+        
+        for (i = 0; i < V; i++)  
+        {  
+            
+            for (j = 0; j < V; j++)  
+            {  
+          
+                if (dist[i][k] + dist[k][j] < dist[i][j])  
+                    dist[i][j] = dist[i][k] + dist[k][j];  
+            }  
+        }  
+    }  
+  
+    
+    printSolution(dist);  
+}  
+  
+void printSolution(int dist[][V])  
+{  
+    cout<<"The following matrix shows the shortest distances"
+            " between every pair of vertices \n";  
+    for (int i = 0; i < V; i++)  
+    {  
+        for (int j = 0; j < V; j++)  
+        {  
+            if (dist[i][j] == INF)  
+                cout<<"INF"<<"     ";  
+            else
+                cout<<dist[i][j]<<"     ";  
+        }  
+        cout<<endl;  
+    }  
+}  
+  
+
+int main()  
+{  
+	 cout<<"Enter The Matrix Values\n";
+    int graph[V][V] ;
+    int i,j;
+    for(i=0;i<V;i++)
+    {
+    	for(j=0;j<V;j++)
+    	{
+    		cin>>graph[i][j];
+    	}
+    }
+    
+ 
+  
+ 
+    floydWarshall(graph);  
+    return 0;  
+}  
+/*------output-------------
+Enter The Matrix Values
+0       2	99	1	8
+6	0	3	2	99
+99	99	0	4	99
+99	99	2	0	3
+3	99	99	99	0
+The following matrix shows the shortest distances between every pair of vertices 
+0     2     3     1     4     
+6     0     3     2     5     
+10     12     0     4     7     
+6     8     2     0     3     
+3     5     6     4     0 
